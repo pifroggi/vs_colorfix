6fe3bb53f5db665f934723bc2d51a0728f401ed3
pifroggi <pifroggimc@gmail.com>
2026-09-16 21:46:01 +0200

    add type hints

--- vs_colorfix/guided_color_fix.py
+++ vs_colorfix/guided_color_fix.py
@@ -54,7 +54,7 @@ def _vsmlrt_inference(clip, ref, backend="ncnn", num_streams=1, gpu_id=0, engine
     return out
 
 
-def guided_color_fix(clip, ref, planes=None, backend="tensorrt", num_streams=1, gpu_id=0, engine_folder=None):
+def guided_color_fix(clip: vs.VideoNode, ref: vs.VideoNode, planes: list[int] | int | None = None, backend: str = "tensorrt", num_streams: int = 1, gpu_id: int = 0, engine_folder: str | None = None) -> vs.VideoNode:
     """Fixes color shift based on a reference clip. This approach is guided by a trained AI model that can intelligently transfer colors while avoiding the typical
         bleed/bloom that can occur in the Average or Wavelet Color Fix, but is much slower.
 
@@ -82,7 +82,7 @@ def guided_color_fix(clip, ref, planes=None, backend="tensorrt", num_streams=1, 
     if ref.format.id == vs.PresetVideoFormat.NONE or ref.width == 0 or ref.height == 0:
         raise TypeError("vs_colorfix.guided: Ref must have constant format and dimensions.")
     if vs.__version__.release_major >= 80 and (clip.gpu_resident or ref.gpu_resident):
-        raise ValueError("vs_colorfix.guided: GPU based input clips are not supported yet. Please transfer both input clips to CPU first.")
+        raise ValueError("vs_colorfix.guided: GPU based input clips are not supported yet. Please download both input clips to CPU first.")
     if clip.format.sample_type != vs.FLOAT:
         raise ValueError("vs_colorfix.guided: Input clips must be in float format.")
     if clip.format.id != ref.format.id:
