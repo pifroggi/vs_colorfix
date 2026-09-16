d97e89b7eba63c45f5dac2054bc5071d7f5ba63f
pifroggi <pifroggimc@gmail.com>
2026-09-16 21:44:43 +0200

    add type hints

--- vs_colorfix/wavelet_color_fix.py
+++ vs_colorfix/wavelet_color_fix.py
@@ -117,7 +117,7 @@ def _wavelet_color_fix_atwt(clip, ref, wavelets, planes):
     return core.std.ShufflePlanes(clips, [0] * num_planes, clip_format.color_family)
 
 
-def wavelet_color_fix(clip, ref, wavelets=4, planes=None, backend="ncnn", num_streams=2, gpu_id=0, engine_folder=None):
+def wavelet_color_fix(clip: vs.VideoNode, ref: vs.VideoNode, wavelets: int = 4, planes: list[int] | int | None = None, backend: str = "ncnn", num_streams: int = 2, gpu_id: int = 0, engine_folder: str | None = None) -> vs.VideoNode:
     """Fixes color shift by converting into wavelets, then matching the average color of a clip to a reference clip. Works similarly to `average()`, but more accurate for larger color differences, at the cost of more computation.
 
     Args:
@@ -146,7 +146,7 @@ def wavelet_color_fix(clip, ref, wavelets=4, planes=None, backend="ncnn", num_st
     if ref.format.id == vs.PresetVideoFormat.NONE or ref.width == 0 or ref.height == 0:
         raise TypeError("vs_colorfix.wavelet: Ref must have constant format and dimensions.")
     if vs.__version__.release_major >= 80 and (clip.gpu_resident or ref.gpu_resident):
-        raise ValueError("vs_colorfix.wavelet: GPU based input clips are not supported yet. Please transfer both input clips to CPU first.")
+        raise ValueError("vs_colorfix.wavelet: GPU based input clips are not supported yet. Please download both input clips to CPU first.")
     if clip.format.id != ref.format.id:
         raise ValueError("vs_colorfix.wavelet: Clip and ref must have the same format.")
     if clip.num_frames != ref.num_frames:
